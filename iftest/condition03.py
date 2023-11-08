c2a718d21734e2a9660ce4c32b64eab3dadc69ce
ryguy270 <ryguy270@gmail.com>
2023-11-08 19:19:41 +0000

    working on if, elif, else logic

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-hostname = input("What value should we set for hostname?")
 ## Notice how the next line has changed
 ## here we use the str.lower() method to return a lowercase string
-if hostname() == "mtg":
+if (input("What value should we set for hostname?"))() == "mtg":
     print("The hostname was found to be mtg")
-elif hostname.lower() == "MTG":
+if (input("What value should we set for hostname?"))() == "MTG":
     print("The hostname was found to be something crazy")
     
